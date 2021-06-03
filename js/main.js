e15094accaeb1ba0ef30fac68f1fee6f732c55dd
tatyana.minina <tatyana.minina@travelline.ru>
2021-06-03 21:57:31 +0300

    Добавляет две функции для генерации чисел: для целых и для чисел с плавающей запятой

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
-function getRandomNumber(min, max, round = 0) {
-  if (typeof min !== 'number' || typeof max !== 'number' || typeof round !== 'number' || min > max || min < 0 || max < 0 || round < 0 || round % 1 !== 0) {
+function getRandomInteger(min, max) {
+  if (min > max || min < 0 || max < 0) {
+    return NaN;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (min === max) {
+    return min;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomFloat(min, max, round) {
+  if (min > max || min < 0 || max < 0 || round < 1 || round % 1 !== 0) {
     return NaN;
   }
 
   if (min === max) {
-    if (round === 0) {
-      return (min % 1 !== 0) ? NaN : min;
-    }
-
     return +min.toFixed(round);
-  }
-
-  if (round === 0) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   return +(Math.random() * (max - min) + min).toFixed(round);
 }
 
-getRandomNumber(0, 1, 2);
+getRandomInteger(0, 1);
+getRandomFloat(0, 1, 2);
