2d68b79188e77b224c96eaa7d4e95ae5c27f9e14
tatyana.minina <tatyana.minina@travelline.ru>
2021-06-06 18:34:10 +0300

    Исправляет ошибки после проверки ESLint

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-'use strict';
+import {getRandomPositiveInteger} from '/js/utils/get-random-positive-integer.js';
+import {getRandomPositiveFloat} from '/js/utils/get-random-positive-float.js';
 
 const AD_NAMES = [
   'ОливаАрт',
@@ -10,7 +11,7 @@ const AD_NAMES = [
   'Ривьера',
   'Сальвадор',
   'SkyPort',
-  'СКИ ИНН'
+  'СКИ ИНН',
 ];
 const AD_DESCRIPTIONS = [
   'Новый современный отель в Алуште',
@@ -22,7 +23,7 @@ const AD_DESCRIPTIONS = [
   '«Ривьера» - четырехзвездочный отель в центре Казани на набережной реки Казанка',
   'Отличный отель второй пляжной линии на Толстом мысе',
   'Отель SKYPORT 4* расположен в 300 метрах от Международного аэропорта «Толмачево»',
-  'Самое сердце горного курорта «Роза Хутор» на высоте 1170 метров'
+  'Самое сердце горного курорта «Роза Хутор» на высоте 1170 метров',
 ];
 
 const AD_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
@@ -32,61 +33,37 @@ const AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'con
 const AD_PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
 const AD_COUNT = 10;
 
-const getRandomInteger = (min, max) => {
-  if (min > max || min < 0 || max < 0) {
-    return NaN;
-  }
+function getRandomArrayElement(array) {
+  return array[getRandomPositiveInteger(0, array.length - 1)];
+}
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  if (min === max) {
-    return min;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-const getRandomFloat = (min, max, round) => {
-  if (min > max || min < 0 || max < 0 || round < 1 || round % 1 !== 0) {
-    return NaN;
-  }
-
-  if (min === max) {
-    return +min.toFixed(round);
-  }
-
-  return +(Math.random() * (max - min) + min).toFixed(round);
-};
-const getRandomArrayElement = (array) => {
-  return array[getRandomInteger(0, array.length - 1)];
-};
-const getRandomArrayElements = (array) => {
-  const count = getRandomInteger(0, array.length - 1);
+function getRandomArrayElements(array) {
+  const count = getRandomPositiveInteger(0, array.length - 1);
   const shuffledArray = array.sort(() => 0.5 - Math.random());
 
   return shuffledArray.slice(0, count);
-};
+}
 
 const createAd = (index) => {
-  const latValue = getRandomFloat(35.65000, 35.70000, 5);
-  const lngValue = getRandomFloat(139.70000, 139.80000, 5);
+  const latValue = getRandomPositiveFloat(35.65000, 35.70000, 5);
+  const lngValue = getRandomPositiveFloat(139.70000, 139.80000, 5);
 
   return {
     author: {
-      avatar: (index <= 8) ? 'img/avatars/user0' + index + '.png' : ''
+      avatar: (index <= 8) ? `img/avatars/user0${index}.png` : '',
     },
     offer: {
       title: AD_NAMES[index],
-      address: latValue + ', ' + lngValue,
-      price: getRandomInteger(0, 10000),
+      address: `${latValue}, ${lngValue}`,
+      price: getRandomPositiveInteger(0, 10000),
       type: getRandomArrayElement(AD_TYPES),
-      rooms: getRandomInteger(0, 10),
-      guests: getRandomInteger(0, 5),
+      rooms: getRandomPositiveInteger(0, 10),
+      guests: getRandomPositiveInteger(0, 5),
       checkin: getRandomArrayElement(AD_CHECKIN_TIMES),
       checkout: getRandomArrayElement(AD_CHECKOUT_TIMES),
       features: getRandomArrayElements(AD_FEATURES),
@@ -95,11 +72,12 @@ const createAd = (index) => {
     },
     location: {
       lat: latValue,
-      lng: lngValue
-    }
-  }
+      lng: lngValue,
+    },
+  };
 };
 
 const ads = new Array(AD_COUNT).fill(null).map((value, index) => createAd(index + 1));
 
+// eslint-disable-next-line no-console
 console.log(ads);
