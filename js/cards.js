bf9ce1c334e75384b9282b4528c68cbe517629b0
tatyana.minina <tatyana.minina@travelline.ru>
2021-06-19 22:13:50 +0300

    Исправляет название переменной

--- js/cards.js
+++ js/cards.js
@@ -5,64 +5,64 @@ const map = document.querySelector('#map-canvas');
 const cardTemplate = document.querySelector('#card').content;
 
 const setSimpleProperties = (card, info) => {
-  const cardProperties = {
+  const cardProperty = {
     title: {
       initValue: info.offer.title,
       setValue: info.offer.title,
       querySelector: '.popup__title',
-      property: 'textContent',
+      attribute: 'textContent',
     },
     address: {
       initValue: info.offer.address,
       setValue: info.offer.address,
       querySelector: '.popup__text--address',
-      property: 'textContent',
+      attribute: 'textContent',
     },
     price: {
       initValue: info.offer.address,
       setValue: `${info.offer.price} <span>₽/ночь</span>`,
       querySelector: '.popup__text--price',
-      property: 'innerHTML',
+      attribute: 'innerHTML',
     },
     type: {
       initValue: info.offer.type,
       setValue: AD_TYPES_RU[info.offer.type],
       querySelector: '.popup__type',
-      property: 'textContent',
+      attribute: 'textContent',
     },
     capacity: {
       initValue: info.offer.rooms && info.offer.guests,
       setValue: `${info.offer.rooms} комнаты для ${info.offer.guests} гостей`,
       querySelector: '.popup__text--capacity',
-      property: 'textContent',
+      attribute: 'textContent',
     },
     time: {
       initValue: info.offer.checkin && info.offer.checkout,
       setValue: `Заезд после ${info.offer.checkin}, выезд до ${info.offer.checkout}`,
       querySelector: '.popup__text--time',
-      property: 'textContent',
+      attribute: 'textContent',
     },
     description: {
       initValue: info.offer.description,
       setValue: info.offer.description,
       querySelector: '.popup__description',
-      property: 'textContent',
+      attribute: 'textContent',
     },
     avatar: {
       initValue: info.author.avatar,
       setValue: info.author.avatar,
       querySelector: '.popup__avatar',
-      property: 'src',
+      attribute: 'src',
     },
   };
 
-  for (const propertyName in cardProperties) {
-    const cardProperty = cardProperties[propertyName];
+  for (const propertyName in cardProperty) {
+    const property = cardProperty[propertyName];
 
-    if (cardProperty.initValue) {
-      card.querySelector(cardProperty.querySelector)[cardProperty.property] = cardProperty.setValue;
+    if (property.initValue) {
+      card.querySelector(property.querySelector)[property.attribute] = property.setValue;
     } else {
-      card.querySelector(cardProperty.querySelector).remove();
+      card.querySelector(property.querySelector).remove();
     }
   }
 };
