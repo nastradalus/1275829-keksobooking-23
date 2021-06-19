2a9c3bb79e26cb62c31cb991318623c64019fb74
tatyana.minina <tatyana.minina@travelline.ru>
2021-06-19 22:09:43 +0300

    Исправляет формат переменной

--- js/cards.js
+++ js/cards.js
@@ -5,66 +5,60 @@ const map = document.querySelector('#map-canvas');
 const cardTemplate = document.querySelector('#card').content;
 
 const setSimpleProperties = (card, info) => {
-  const cardProperties = [
-    {
-      name: 'title',
+  const cardProperties = {
+    title: {
       initValue: info.offer.title,
       setValue: info.offer.title,
       querySelector: '.popup__title',
       property: 'textContent',
     },
-    {
-      name: 'address',
+    address: {
       initValue: info.offer.address,
       setValue: info.offer.address,
       querySelector: '.popup__text--address',
       property: 'textContent',
     },
-    {
-      name: 'price',
+    price: {
       initValue: info.offer.address,
       setValue: `${info.offer.price} <span>₽/ночь</span>`,
       querySelector: '.popup__text--price',
       property: 'innerHTML',
     },
-    {
-      name: 'type',
+    type: {
       initValue: info.offer.type,
       setValue: AD_TYPES_RU[info.offer.type],
       querySelector: '.popup__type',
       property: 'textContent',
     },
-    {
-      name: 'capacity',
+    capacity: {
       initValue: info.offer.rooms && info.offer.guests,
       setValue: `${info.offer.rooms} комнаты для ${info.offer.guests} гостей`,
       querySelector: '.popup__text--capacity',
       property: 'textContent',
     },
-    {
-      name: 'time',
+    time: {
       initValue: info.offer.checkin && info.offer.checkout,
       setValue: `Заезд после ${info.offer.checkin}, выезд до ${info.offer.checkout}`,
       querySelector: '.popup__text--time',
       property: 'textContent',
     },
-    {
-      name: 'description',
+    description: {
       initValue: info.offer.description,
       setValue: info.offer.description,
       querySelector: '.popup__description',
       property: 'textContent',
     },
-    {
-      name: 'avatar',
+    avatar: {
       initValue: info.author.avatar,
       setValue: info.author.avatar,
       querySelector: '.popup__avatar',
       property: 'src',
     },
-  ];
+  };
 
-  for (const cardProperty of cardProperties) {
+  for (const propertyName in cardProperties) {
+    const cardProperty = cardProperties[propertyName];
+
     if (cardProperty.initValue) {
       card.querySelector(cardProperty.querySelector)[cardProperty.property] = cardProperty.setValue;
     } else {
